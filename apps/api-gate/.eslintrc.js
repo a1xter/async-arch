f6794c45b778f787e5265d111d38d0e97f1c0f80
alex_tereshkov <alex@teeho.cc>
2022-09-25 17:59:01 +0700

    - added monorepo - added web react - connected api to web

---
+++ apps/api-gate/.eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  root: true,
+  extends: ["custom"],
+  env: {
+    node: true,
+    jest: true
+  },
+  parserOptions: {
+    project: 'tsconfig.json',
+    tsconfigRootDir: __dirname,
+    sourceType: 'module'
+  }
+};
